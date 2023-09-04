c1aefb3a69ad0361bca3bb49e0323d1c52aa4771
Julien Donque <julien@lesfabricants.io>
2023-09-04 14:45:40 +0200

    added build command

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ console.log(figlet.textSync("Realtimate"));
 
 const program = new Command();
 program
+  .name("realtimate")
   .version(process.env.npm_package_version || "")
   .description(process.env.npm_package_description || "")
   .command("init", "Init the project")
   .command("run", "The command to run dev server")
+  .command("build", "The command to build the app")
   .option("-w, --watch", "Watch for changes")
   .command("new <app>", "Create a new Realm app");
 
